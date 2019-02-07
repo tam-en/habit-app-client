5dae5787326e15cd428bb660e68b7f5c9f620779
tam-en <tamis@leftelbow.com>
2019-02-07 15:14:35 -0800

    adding habit id parameter on fetch in HabitDetails

--- src/HabitDetail.js
+++ src/HabitDetail.js
@@ -30,7 +30,7 @@ class HabitDetail extends Component {
     habit: this.props.habit,
     dayData: dayData
   }
-  fetch(SERVER_URL+'/habits/completions/'+this.props.user.id, {
+  fetch(SERVER_URL+'/habits/completions/'+this.props.user.id+'/'+this.props.habit.id, {
       method: 'PUT',
       body: JSON.stringify(bodyToSend), // data to send to server
       headers: {
